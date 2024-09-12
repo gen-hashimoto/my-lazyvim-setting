e7125e4741259b222cb838923f33d521ff5909f3
kpis-hashimoto-gen <hashimoto.gen.kpis@gmail.com>
2024-09-12 22:32:39 +0900

    add: winbar, gruvbox

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,4 +6,4 @@ local opt = vim.opt
 
 opt.relativenumber = false -- Relative line numbers
 opt.wrap = true -- line wrap
-opt.winbar = "%=%m %f" -- path
+opt.winbar = "%=%m %f" -- show file path on the winbar
